85758ebf9bd1d1c2d99ca0adf05da9f9a9648421
Matheus Moselli <mat.moselli@hotmail.com>
2020-10-10 11:56:03 -0300

    Creating Position Class

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Board;
+using System;
 
 namespace Chess
 {
@@ -6,7 +7,11 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + p);
+
+            Console.ReadLine();
         }
     }
 }
